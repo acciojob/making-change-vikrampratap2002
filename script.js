0fbd8c94083e58ccb72d012950e71d12a19a55d3
Vikram pratap <122038154+vikrampratap2002@users.noreply.github.com>
2026-03-01 14:51:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const makeChange = (c) => {
   // your name here
+	let q = Math.floor(c/25);
+	c = c%25;
+	let d = Math.floor(c/10);
+	c = c%10;
+	let n = Math.floor(c/5);
+	c = c%5;
+	let p = c;
+	return {
+		q: q,
+		d:d,
+		n:n,
+		p:p
+	};
+	
 };
 
 // Do not the change the code below
